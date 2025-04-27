a908b90cff16dacb2948f631a92f8b2cfd4c9de5
facu-programer <loscompartidosfayfe@gmail.com>
2025-04-27 00:15:45 -0300

    Add files via upload

---
+++ pythom.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'pythom.exe' with Python DLL being found.
+set PATH=c:\users\usuario\appdata\local\programs\python\python313;%PATH%
+set PYTHONHOME=C:\Users\Usuario\AppData\Local\Programs\Python\Python313
+set NUITKA_PYTHONPATH=C:\Users\Usuario\Desktop\python\kodland python pro\memes;C:\Users\Usuario\AppData\Local\Programs\Python\Python313\DLLs;C:\Users\Usuario\AppData\Local\Programs\Python\Python313\Lib;C:\Users\Usuario\AppData\Local\Programs\Python\Python313;C:\Users\Usuario\AppData\Roaming\Python\Python313\site-packages;C:\Users\Usuario\AppData\Roaming\Python\Python313\site-packages\win32;C:\Users\Usuario\AppData\Roaming\Python\Python313\site-packages\win32\lib;C:\Users\Usuario\AppData\Roaming\Python\Python313\site-packages\Pythonwin;C:\Users\Usuario\AppData\Local\Programs\Python\Python313\Lib\site-packages;C:\Users\Usuario\AppData\Local\Programs\Python\Python313\Lib\site-packages\setuptools\_vendor
+"%~dp0pythom.exe" %*
